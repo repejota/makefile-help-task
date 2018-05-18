237ac991480642bd515f64c0d463730b46f56d1e
Raül Pérez <repejota@gmail.com>
2018-05-18 10:20:30 +0200

    Remove old tests º

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-# simple
-EXPECTED="help:*"
-OUTPUT="$(make -f tests/Makefile.simple)"
-if [[ ${OUTPUT} != ${EXPECTED} ]]; then
-    echo "FAIL simple: expected: '${EXPECTED}' but got: '${OUTPUT}'"
-fi
-
